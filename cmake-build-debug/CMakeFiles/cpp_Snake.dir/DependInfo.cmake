22581f940a93b8075ef4f824326c11eeef50c4cd
Banigan, Jake Pavel <jbanigan3@gatech.edu>
2024-06-03 17:55:11 -0500

    Cmake commit

---
+++ cmake-build-debug/CMakeFiles/cpp_Snake.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/jake/OneDrive/Desktop/School/cpp_Snake/src/engine.cpp" "CMakeFiles/cpp_Snake.dir/src/Engine.cpp.obj" "gcc" "CMakeFiles/cpp_Snake.dir/src/Engine.cpp.obj.d"
+  "C:/Users/jake/OneDrive/Desktop/School/cpp_Snake/src/draw.cpp" "CMakeFiles/cpp_Snake.dir/src/draw.cpp.obj" "gcc" "CMakeFiles/cpp_Snake.dir/src/draw.cpp.obj.d"
+  "C:/Users/jake/OneDrive/Desktop/School/cpp_Snake/src/input.cpp" "CMakeFiles/cpp_Snake.dir/src/input.cpp.obj" "gcc" "CMakeFiles/cpp_Snake.dir/src/input.cpp.obj.d"
+  "C:/Users/jake/OneDrive/Desktop/School/cpp_Snake/src/main.cpp" "CMakeFiles/cpp_Snake.dir/src/main.cpp.obj" "gcc" "CMakeFiles/cpp_Snake.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cpp_Snake.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_Snake.dir/src/draw.cpp.obj"
+  "CMakeFiles/cpp_Snake.dir/src/draw.cpp.obj.d"
+  "CMakeFiles/cpp_Snake.dir/src/engine.cpp.obj"
+  "CMakeFiles/cpp_Snake.dir/src/engine.cpp.obj.d"
+  "CMakeFiles/cpp_Snake.dir/src/input.cpp.obj"
+  "CMakeFiles/cpp_Snake.dir/src/input.cpp.obj.d"
+  "CMakeFiles/cpp_Snake.dir/src/main.cpp.obj"
+  "CMakeFiles/cpp_Snake.dir/src/main.cpp.obj.d"
+  "cpp_Snake.exe"
+  "cpp_Snake.exe.manifest"
+  "cpp_Snake.pdb"
+  "libcpp_Snake.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_Snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
